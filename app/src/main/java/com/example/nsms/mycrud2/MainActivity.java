d753be015965f3f8e339588717374430914bbc5c
unknown <nursyatirahsaad@gmail.com>
2018-12-27 15:08:08 +0800

    first commit

---
+++ app/src/main/java/com/example/nsms/mycrud2/MainActivity.java
@@ -0,0 +1,127 @@
+package com.example.nsms.mycrud2;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private EditText editTextName;
+    private EditText editTextDesg;
+    private EditText editTextSal;
+
+    private Button buttonAdd;
+    private Button buttonView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Inisialisasi dari View
+        editTextName = (EditText) findViewById(R.id.editTextName);
+        editTextDesg = (EditText) findViewById(R.id.editTextDesg);
+        editTextSal = (EditText) findViewById(R.id.editTextSalary);
+
+        buttonAdd = (Button) findViewById(R.id.buttonAdd);
+        buttonView = (Button) findViewById(R.id.buttonView);
+
+        //Setting listeners to button
+        buttonAdd.setOnClickListener(this);
+        buttonView.setOnClickListener(this);
+
+    }
+
+    private void addEmployee(){
+
+        final String name = editTextName.getText().toString().trim();
+        final String desg = editTextDesg.getText().toString().trim();
+        final String sal = editTextSal.getText().toString().trim();
+
+        class AddEmployee extends AsyncTask<Void,Void,String>{
+
+            ProgressDialog loading;
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loading = ProgressDialog.show(MainActivity.this,"Menambahkan...","Tunggu...",false,false);
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                loading.dismiss();
+                Toast.makeText(MainActivity.this,s,Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            protected String doInBackground(Void... v) {
+                HashMap<String,String> params = new HashMap<>();
+                params.put(konfigurasi.KEY_EMP_NAMA,name);
+                params.put(konfigurasi.KEY_EMP_POSISI,desg);
+                params.put(konfigurasi.KEY_EMP_GAJIH,sal);
+
+                RequestHandler rh = new RequestHandler();
+                String res = rh.sendPostRequest(konfigurasi.URL_ADD, params);
+                return res;
+            }
+        }
+
+        AddEmployee ae = new AddEmployee();
+        ae.execute();
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == buttonAdd){
+            addEmployee();
+        }
+
+        if(v == buttonView){
+            startActivity(new Intent(this,TampilSemuaPgw.class));
+        }
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
